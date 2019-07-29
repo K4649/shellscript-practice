a823b6f4612cc15f467a30300673351f4ceb867f
K4649 <52810143+K4649@users.noreply.github.com>
2019-07-29 11:29:46 +0900

    Add files via upload

---
+++ test2-2.sh
@@ -0,0 +1,12 @@
+#WHILE文でリストファイル（）を読み込んでループ処理を実施し、
+#リストファイルに書かれている文字列を全行をechoコマンドで表示する
+
+touch shell_syokyu_02.lst
+
+while read line
+do 
+echo $line
+done < shell_syokyu_02.lst
+
+#ファイルの中身が表示される
+
