078a841b7e287c415364339c4d35c6d32f8d426b
K4649 <52810143+K4649@users.noreply.github.com>
2019-07-31 10:18:49 +0900

    Update test2-2.sh

--- test2-2.sh
+++ test2-2.sh
@@ -2,11 +2,14 @@
 #リストファイルに書かれている文字列を全行をechoコマンドで表示する
 #!/bin/sh
 touch shell_syokyu_02.lst
+#ファイルを作成
 
 while read line
+#while read lineを使って1行ずつ読み込み、echoで表示
 do 
 echo $line
 done < shell_syokyu_02.lst
+#結果をリダイレクトでファイルから取得
 
 #ファイルの中身が表示される
 
