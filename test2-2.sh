c3063ff2bfbb4f392622ae8c2484f05d16ff2a0a
K4649 <52810143+K4649@users.noreply.github.com>
2019-07-31 11:52:26 +0900

    Update test2-2.sh

--- test2-2.sh
+++ test2-2.sh
@@ -1,8 +1,9 @@
 #WHILE文でリストファイル（）を読み込んでループ処理を実施し、
 #リストファイルに書かれている文字列を全行をechoコマンドで表示する
+
 #!/bin/sh
 touch shell_syokyu_02.lst
-#ファイルを作成
+#touchでファイルを作成
 
 while read line
 #while read lineを使って1行ずつ読み込み、echoで表示
