fbe31a90cf1752c711ded4fddacebcafc74094bf
K4649 <52810143+K4649@users.noreply.github.com>
2019-07-31 10:52:20 +0900

    Update test6.sh

--- test6.sh
+++ test6.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-ls -l $1 >　/dev/null 2>&1
-if [$? = 0 ]
+ls -l $1 > /dev/null 2>&1
+if [ $? = 0 ]
 then
- ls　-l $1
-exit 0
+ ls -l $1
+ exit 0
 else
  echo "エラー終了"
-exit 1
+ exit 1
 fi
 
 #ファイルのパスを引数で指定して、
